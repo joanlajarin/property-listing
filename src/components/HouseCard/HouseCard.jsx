20c126aceb45c78fde9c3fa7e93021ffe076cb8a
Joan Lajarin <joanlajarinreinares@gmail.com>
2024-02-08 14:12:32 +0100

    Component HouseCard and listHouseCard

--- src/components/HouseCard/HouseCard.jsx
+++ src/components/HouseCard/HouseCard.jsx
@@ -1,24 +1,41 @@
 import React from "react"
+import './HouseCard.css'
+import home from '../../../public/images/Home_duotone.svg'
+import user from '../../../public/images/User_alt_duotone.svg'
+import star from '../../../public/images/Starfill.svg'
 
-
-export default function HouseCard() {
-    const houseExample =  {
-        "id": 1,
-        "title": "Nordic Retreat with Stunning Views",
-        "description": "Immerse yourself in the beauty of the Nordic landscape at this sleek and stylish cabin. With floor-to-ceiling windows and a spacious deck, you'll have breathtaking views of the surrounding mountains.",
-        "price": 250,
-        "rating": 4.9,
-        "superhost": true,
-        "location": "Norway",
-        "capacity": {
-          "people": 3,
-          "bedroom": 2
-        },
-        "image": "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/property-challenge/thumbnail-1.jpg"
-      }
+export default function HouseCard(props) {
+    const {house} =  props
     return (
-        <div className="house-card">
+        <div key={house.id} className="house-card">
+            <div className="image-container">
+                <img className="img-house-card" src={house.image}></img>
+            </div>
+            <div className="info-house-card">
+                <span className="title-house-card">{house.title}</span>
+                <p className="description-house-card">{house.description}</p>
+                <div className="bedroom-guest-house-card">
+                    <div className="bedroom-house-card">
+                        <img src={home}></img>
+                        <span className="capacity-house-card">{house.capacity.bedroom} bedroom</span>
+                    </div>
+                    <div className="guest-house-card">
+                        <img src={user}></img>
+                        <span className="guest-house-card">{house.capacity.people} guest</span>             
+                    </div>
+                </div>
+                <div className="price-stars-house-card">
+                    <div className="price-house-card">
+                        <span>${house.price}</span>
+                        <span>/night</span>             
 
+                    </div>
+                    <div className="stars-house-card">
+                        <img src={star}></img>
+                        <span>{house.rating}</span>             
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/HouseListing/HouseListing.jsx
+++ src/components/HouseListing/HouseListing.jsx
@@ -1,12 +1,27 @@
 import React from "react"
 import GetHouses from "../../services/GetHouses"
+import HouseCard from "../HouseCard/HouseCard"
+import './HouseListing.css'
 
 export default function HouseListing(){
     const houses = GetHouses() 
+    console.log("houses " + houses)
+
 
     return (
-        <div className="house-listing">
-            <h1>Over 200 stays</h1>
+        <section className="main-house-listing">
+            <div className="house-listing">
+            <h1 className="title-house-listing">Over 200 stays</h1>
+            <div className="list-houses">
+            {
+                houses ? ( 
+                    houses.map(house => <HouseCard key={house.id }house={house}/>
+                )) : (
+                    <p>Loading...</p>
+                  )
+            }
+            </div>
         </div>
+        </section>
     )
 }
